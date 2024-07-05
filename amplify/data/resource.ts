c99167d93f3348a10ef377c352ebcc28953e3910
SMR-AIT <youremail@yourdomain.comsmrait@outlook.com>
2024-07-05 17:58:52 +0800

    correct data resource (multiple define data)

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,30 +6,12 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
-const schema_todo = a.schema({
+const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
-});
-
-export type Schema_todo = ClientSchema<typeof schema_todo>;
-
-export const data = defineData({
-  schema: schema_todo,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
-});
-
-// ============================================================================
-
-const schema_docbucket = a.schema({
   Doc: a
     .model({
       // id: a.id().required(),
@@ -39,25 +21,26 @@ const schema_docbucket = a.schema({
     .authorization((allow) => [allow.authenticated()]),
 });
 
-export type Schema_docbucket = ClientSchema<typeof schema_docbucket>;
+export type Schema = ClientSchema<typeof schema>;
 
-export const data_docbucket = defineData({
-  schema: schema_docbucket,
+export const data = defineData({
+  schema: schema,
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
-
+    // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
   },
 });
 
+
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
--- components/Todos.tsx
+++ components/Todos.tsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
-import type { Schema_todo } from "@/amplify/data/resource";
+import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
 import Link from 'next/link';
 
 Amplify.configure(outputs);
-const client = generateClient<Schema_todo>();
+const client = generateClient<Schema>();
 
 const Todos = () => {
-    const [todos, setTodos] = useState<Array<Schema_todo["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
 
     useEffect(() => {
