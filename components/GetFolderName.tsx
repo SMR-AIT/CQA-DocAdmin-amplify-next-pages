84fa4badf4af90ee016ac1c7e94c376cb7cd599d
josephorng <josephorng@hotmail.com>
2024-07-10 07:30:21 +0800

    add material ui and sticky head

--- components/GetFolderName.tsx
+++ components/GetFolderName.tsx
@@ -1,21 +1,22 @@
 function getValidFolderName() {
-    const validFolderNameRegex = /^[a-zA-Z0-9_\-\u4e00-\u9fa5 ]+$/;
+  const validFolderNameRegex = /^[a-zA-Z0-9_\-\u4e00-\u9fa5 ]+$/;
 
-    while (true) {
-        const folderName = window.prompt("Enter a valid folder name:");
+  while (true) {
+    const folderName = window.prompt("Enter a valid folder name:");
 
-        if (folderName === null) {
-            // User canceled the prompt
-            return null;
-        } else if (validFolderNameRegex.test(folderName)) {
-            // Folder name is valid
-            return folderName;
-        } else {
-            // Folder name is invalid, show an alert and prompt again
-            alert("Invalid folder name. Please use only letters, numbers, spaces, hyphens, and underscores.");
-        }
+    if (folderName === null) {
+      // User canceled the prompt
+      return null;
+    } else if (validFolderNameRegex.test(folderName)) {
+      // Folder name is valid
+      return folderName;
+    } else {
+      // Folder name is invalid, show an alert and prompt again
+      alert(
+        "Invalid folder name. Please use only letters, numbers, spaces, hyphens, and underscores."
+      );
     }
+  }
 }
-
 
 export default getValidFolderName;
