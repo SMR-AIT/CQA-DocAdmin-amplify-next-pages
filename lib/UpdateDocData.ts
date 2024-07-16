6a7ba51a0d56349c2c926ecb6912974d622327c8
SMR-AIT <youremail@yourdomain.comsmrait@outlook.com>
2024-07-16 18:15:36 +0800

    uopdate

--- lib/UpdateDocData.ts
+++ lib/UpdateDocData.ts
@@ -7,26 +7,62 @@ import { list } from 'aws-amplify/storage';
 
 
 Amplify.configure(outputs);
-// const root = "Doc/";
 
-// Generating the client
-const client = generateClient<Schema>({
-    authMode: "apiKey",
-});
-// client.queries.buildVDB({name:''})
-type Doc = Schema["Doc"]["type"];
+const client = generateClient<Schema>({ authMode: "apiKey", });
+
+const file_ebd = '/Embeddings.npy'
+const file_txt = '/Extract_doc_squeezed.txt'
+const file_sum = '/Summary_chatgpt.txt'
+
+function removeFileExtension(filePath:string) {
+    const lastDotIndex = filePath.lastIndexOf('.');
+    if (lastDotIndex === -1) {
+      return filePath; // No extension found
+    }
+    return filePath.slice(0, lastDotIndex);
+  }
 
 export async function updateDocStatus() {
     const s3_result = await list({ path: 'txt/', });
-    const s3_keys = s3_result['items'].map((item) => item['path'].slice(5))
+    const s3_keys = s3_result['items'].map((item) => item['path'].slice(4))
     console.log(s3_keys)
+    
+    const embeds = s3_keys.filter((key) => key.endsWith(file_ebd)).map((file)=>file.slice(0, -file_ebd.length))
+    const txts = s3_keys.filter((key) => key.endsWith(file_txt)).map((file)=>file.slice(0, -file_txt.length))
+    const summaries = s3_keys.filter((key) => key.endsWith(file_sum)).map((file)=>file.slice(0, -file_sum.length))
+    console.log(embeds)
+    console.log(txts)
+    console.log(summaries)
 
     const data_result = await client.models.Doc.list()
-    const data_ids = data_result['data'].map((data) => data.id)
+    const data_ids = data_result['data'].map((data) => removeFileExtension(data.id))
     console.log(data_ids)
 
-    // for (const key in s3_keys){
-    //     data_ids.filter((id)=>id.startsWith(key))
-    //     client.models.Doc.update({ id: '123', statusText: 'Done' });
-    // }
+    for (const id of data_ids){
+        console.log(id)
+        if (txts.includes(id)){
+            try{
+                client.models.Doc.update({ id: id, statusText: 'Done' });
+                console.log('succeed')
+            }catch(error){
+                console.log(error);
+            }
+        }else if(embeds.includes(id)){
+            try{
+                client.models.Doc.update({ id: id, statusEmbed: 'Done' });
+                console.log('succeed')
+            }catch(error){
+                console.log(error);
+            }
+        }else if(summaries.includes(id)){
+            try{
+                client.models.Doc.update({ id: id, statusSummary: 'Done' });
+                console.log('succeed')
+            }catch(error){
+                console.log(error);
+            }
+        }else{
+            console.log('None match')
+        }
+    }
 }
