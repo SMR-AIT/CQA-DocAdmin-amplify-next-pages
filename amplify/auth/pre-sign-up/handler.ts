b50683906d37d53ec39bd8f4ca29d6754fb1031e
SMR-AIT <youremail@yourdomain.comsmrait@outlook.com>
2024-08-08 13:40:58 +0800

    fix presignup error

--- amplify/auth/pre-sign-up/handler.ts
+++ amplify/auth/pre-sign-up/handler.ts
@@ -5,7 +5,7 @@ export const handler: PreSignUpTriggerHandler = async (event) => {
   const email = event.request.userAttributes["email"];
 
   if (
-    !email.endsWith("@smr.com.tw")||!email.endsWith("@mail.smr.com.tw")
+    !email.endsWith("@smr.com.tw")&&!email.endsWith("@mail.smr.com.tw")
   ) {
     throw new Error("Invalid email domain");
   }
